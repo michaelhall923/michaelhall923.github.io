51ca29cfb8a2523a8d17bc0877987b9edeaa3ca0
Michael Hall <michaelhall923@protonmail.com>
2018-12-03 17:00:10 -0500

    Finish the JavaScript gallery

--- _site/js/gallery.js
+++ _site/js/gallery.js
@@ -1,3 +1,26 @@
 function activateGallery() {
-  alert("Hello from the gallery file");
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+      let mainImage = document.querySelector("#gallery-photo img");
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      let newImageAlt = thumbnail.alt;
+
+      mainImage.setAttribute("src", newImageSrc);
+      mainImage.setAttribute("alt", newImageAlt);
+
+      let currentClass = "current";
+      document.querySelector(`.${currentClass}`).classList.remove(currentClass);
+      thumbnail.parentNode.classList.add(currentClass);
+
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+
+      title.innerHTML = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
+    });
+  });
+
 }
