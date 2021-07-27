8a132eb202ed983dde90a1acf8f721451f037d02
michael <michaelhall923@protonmail.com>
2021-07-27 00:15:43 -0400

    Add windup animation

--- js/script.js
+++ js/script.js
@@ -17,5 +17,32 @@ $(document).ready(function() {
         $('#dark-mode-toggle-dark').removeClass('hidden');
         $('body').removeClass('dark-mode');
     }
+
+    let framerate = 60, theta = 0, velocity = 0, acceleration = 0, accelerationConstant = .02, winding = false, windingVelocity = 3, friction = .03;
+    setInterval(animate, 1000/framerate);
+
+    function animate() {
+        if (winding) {
+            velocity = 0;
+            acceleration = 0;
+            theta += windingVelocity;
+            if (theta < 0) theta = modulus(theta, 360);
+        } else {
+            acceleration = -(theta * accelerationConstant);
+            velocity += acceleration;
+            velocity *= (1-friction);
+            theta += velocity;
+        }
+        $('#emblem').css('transform', `rotate(${Math.round(theta)}deg)`);
+    }
+
+    $('#emblem').on('mouseover', function() {
+        winding = true;
+    }).on('mouseleave', function() {
+        winding = false;
+    })
 });
 
+function modulus(a, n) {
+    return ((a % n ) + n ) % n;
+}
