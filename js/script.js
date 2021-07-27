814ddda5ea8f0ca43458095760ac38b7ef14c1d4
michael <michaelhall923@protonmail.com>
2021-07-26 21:50:03 -0400

    Refactor Javacsript

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
 $(document).ready(function() {
     var darkModeEnabled = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    if (darkModeEnabled) toggleDarkMode();
 
-    $('#dark-mode-toggle-dark').on('click', function() {
-        $(this).addClass('hidden');
+    $('#dark-mode-toggle-dark').on('click', toggleDarkMode);
+
+    $('#dark-mode-toggle-light').on('click', toggleLightMode);
+
+    function toggleDarkMode() {
+        $('#dark-mode-toggle-dark').addClass('hidden');
         $('#dark-mode-toggle-light').removeClass('hidden');
         $('body').addClass('dark-mode');
-    });
+    }
 
-    $('#dark-mode-toggle-light').on('click', function() {
-        $(this).addClass('hidden');
+    function toggleLightMode() {
+        $('#dark-mode-toggle-light').addClass('hidden');
         $('#dark-mode-toggle-dark').removeClass('hidden');
         $('body').removeClass('dark-mode');
-    });
-
-    if (darkModeEnabled) $('#dark-mode-toggle-dark').click();
+    }
 });
 
